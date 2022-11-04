be32a4702ebb7ccf6ebf9bd80fdc59687a42a242
danielmacsik <daniel.macsik@stud.uni-corvinus.hu>
2022-11-04 10:45:16 +0100

    tasks/week06/task06

---
+++ DJT601_GY06/DJT601_GY06/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using DJT601_GY06.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJT601_GY06.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
