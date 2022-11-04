c33cff2fde115d2793432cf2c99f73c595cc7f85
danielmacsik <daniel.macsik@stud.uni-corvinus.hu>
2022-11-04 10:39:02 +0100

    tasks/week06/task05

--- DJT601_GY06/DJT601_GY06/Entities/Ball.cs
+++ DJT601_GY06/DJT601_GY06/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DJT601_GY06.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,28 +9,11 @@ using System.Windows.Forms;
 
 namespace DJT601_GY06.Entities
 {
-    public class Toy: Label
+    public class Ball : Toy
     {
-        public Toy()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-        public void MoveBall() 
-        {
-            Left += 1;
         }
     }
 }
